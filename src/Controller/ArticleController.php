cc77c1963f57cfb54bab30a3b1513729d2c0d843
Faty <fatydiiop95@gmail.com>
2021-07-02 11:44:42 +0200

    recuperation bdd avec wildcard

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -12,8 +12,9 @@ class ArticleController
     /**
      * @Route("/article", name="article")
      */
-    public function article()
+    public function article() //public = methode
     {
+        //je retourne une reponse valide pour ma pg d'article puis je retourne HttpFoundation
         return new Response('Bonbon');
     }
 
@@ -24,6 +25,33 @@ class ArticleController
      */
     public function articShow($id)
     {
-        return new Response($id);
+        //ici je crée un tableau comme j'avais récuperé la bdd
+        $articles = [
+            '1' => [
+                "title" => "L'été c'est trop génialeeee",
+                "content" =>"youpi",
+                "id" => 1
+            ],
+            '2' => [
+                "title" => "La chaleur c'est de la merde",
+                "content" =>"blablabla",
+                "id" => 2
+            ],
+            '3' => [
+                "title" => "Avoir des migraine c'est trop cool",
+                "content" =>"blablabla",
+                "id" => 3
+            ],
+            '4' => [
+                "title" => "juju",
+                "content" =>"blablabla",
+                "id" => 4
+            ],
+        ];
+            //cet variable permet la recupe la wildcard
+            $article = $articles[$id];
+
+
+            return new Response($article['title']); //ici je recupere le titre de mon tableau
     }
 }

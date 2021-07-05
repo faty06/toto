86272d189a63aa0f8cd4f7e60e1e02427a778bfb
Faty <fatydiiop95@gmail.com>
2021-07-05 14:43:17 +0200

    exo gabarit

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -11,13 +11,38 @@ use Symfony\Component\Routing\Annotation\Route;
 class ArticleController extends AbstractController
 //tjrs mettre l'extends AbstractController
 {
+    private $articles = [
+        '1' => [
+            "title" => "L'été c'est trop génialeeee",
+            "content" =>"youpi",
+            "id" => 1
+        ],
+        '2' => [
+            "title" => "La chaleur c'est de la merde",
+            "content" =>"blablabla",
+            "id" => 2
+        ],
+        '3' => [
+            "title" => "Avoir des migraine c'est trop cool",
+            "content" =>"blablabla",
+            "id" => 3
+        ],
+        '4' => [
+            "title" => "juju",
+            "content" =>"blablabla",
+            "id" => 4
+        ],
+    ];
+
     /**
-     * @Route("/article", name="article")
+     * @Route("/articles", name="article")
      */
-    public function article() //public = methode
+
+    public function articleList()
     {
         //je retourne une reponse valide pour ma pg d'article puis je retourne HttpFoundation
-        return new Response('Bonbon');
+        //return new Response('articles');
+        return $this->render('article_list.html.twig',['articles' => $this->articles]);
     }
 
     /**
@@ -27,37 +52,10 @@ class ArticleController extends AbstractController
      */
     public function articleShow($id)
     {
-        //ici je crée un tableau comme j'avais récuperé la bdd
-        $articles = [
-            '1' => [
-                "title" => "L'été c'est trop génialeeee",
-                "content" =>"youpi",
-                "id" => 1
-            ],
-            '2' => [
-                "title" => "La chaleur c'est de la merde",
-                "content" =>"blablabla",
-                "id" => 2
-            ],
-            '3' => [
-                "title" => "Avoir des migraine c'est trop cool",
-                "content" =>"blablabla",
-                "id" => 3
-            ],
-            '4' => [
-                "title" => "juju",
-                "content" =>"blablabla",
-                "id" => 4
-            ],
-        ];
-            //cet variable permet la recupe la wildcard "$id"
-            //$article = $articles[$id];
-
-
-            //
             return $this->render('article_show.html.twig', [
-                'article' => $articles[$id]
+            'article' => $this->articles[$id]
             ]);
+           // return new Response('test');
             //la Methode "render  extends AbstractController" permet de recuperer un fichier "twig" qui
             // converti en html puis il envoie une "Responce" HTTP au "Client"
     }
